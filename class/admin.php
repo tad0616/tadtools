61c9ba06d6c42188fb6d202f9a95367e0ec98c0d
mambax7 <mambax7@gmail.com>
2018-03-02 08:10:04 -0500

    cosmetics

--- class/admin.php
+++ class/admin.php
@@ -301,7 +301,7 @@ class ModuleAdmin
         $ret .= "</table>\n";
 
         //        $ret .= "<div align=\"center\">";
-        //        $ret .= "<a href=\"http://www.xoops.org\" target=\"_blank\"><img src=\"" . $path . "xoopsmicrobutton.gif\" alt=\"XOOPS\" title=\"XOOPS\"></a>";
+        //        $ret .= "<a href=\"https://xoops.org\" target=\"_blank\"><img src=\"" . $path . "xoopsmicrobutton.gif\" alt=\"XOOPS\" title=\"XOOPS\"></a>";
         //        $ret .= "</div>";
 
         return $ret;
@@ -324,7 +324,7 @@ class ModuleAdmin
         $ret .= "</tr>\n";
         $ret .= "</table>\n";
         $ret .= "<div align=\"center\">";
-        //        $ret .= "<a href=\"http://www.xoops.org\" target=\"_blank\"><img src=\"" . $path . "xoopsmicrobutton.gif\" alt=\"XOOPS\" title=\"XOOPS\"></a>";
+        //        $ret .= "<a href=\"https://xoops.org\" target=\"_blank\"><img src=\"" . $path . "xoopsmicrobutton.gif\" alt=\"XOOPS\" title=\"XOOPS\"></a>";
         $ret .= "</div>";
         return $ret;
     }
