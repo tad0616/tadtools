fb929be922bdd1a34a47f7476428e3998bc79cbc
tad0616 <tad0616@gmail.com>
2019-04-04 12:00:46 +0800

    修正tcpdf中文檔名下載問題

--- tcpdf/tcpdf.php
+++ tcpdf/tcpdf.php
@@ -7558,8 +7558,8 @@ class TCPDF {
 		}
 		$dest = strtoupper($dest);
 		if ($dest[0] != 'F') {
-			$name = preg_replace('/[\s]+/', '_', $name);
-			$name = preg_replace('/[^a-zA-Z0-9_\.-]/', '', $name);
+			// $name = preg_replace('/[\s]+/', '_', $name);
+			// $name = preg_replace('/[^a-zA-Z0-9_\.-]/', '', $name);
 		}
 		if ($this->sign) {
 			// *** apply digital signature to the document ***
