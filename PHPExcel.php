d506b20d61de94b2ac343b47ae83085afebbe870
mambax7 <mambax7@gmail.com>
2019-04-13 06:50:40 -0400

    __DIR__
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- PHPExcel.php
+++ PHPExcel.php
@@ -27,7 +27,7 @@
 
 /** PHPExcel root directory */
 if (!defined('PHPEXCEL_ROOT')) {
-    define('PHPEXCEL_ROOT', dirname(__FILE__) . '/');
+    define('PHPEXCEL_ROOT', __DIR__ . '/');
     require(PHPEXCEL_ROOT . 'PHPExcel/Autoloader.php');
 }
 
