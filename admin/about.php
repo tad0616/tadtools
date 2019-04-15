79a5079f8317adab4fd560075b685ee486308496
mambax7 <mambax7@gmail.com>
2019-04-15 05:35:56 -0400

    cosmetics3
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ admin/about.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * @copyright    XOOPS Project https://xoops.org/
+ * @license      GNU GPL 2 or later (http://www.gnu.org/licenses/gpl-2.0.html)
+ * @package
+ * @since
+ * @author     XOOPS Development Team
+ */
+require __DIR__ . '/header.php';
+//xoops_cp_header();
+
+$adminObject = \Xmf\Module\Admin::getInstance();
+
+$adminObject->displayNavigation(basename(__FILE__));
+$adminObject::setPaypal('xoopsfoundation@gmail.com');
+$adminObject->displayAbout(false);
+
+require __DIR__ . '/footer.php';
