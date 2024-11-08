75259e58b2f62e05aafde90c3f548dc51baede97
tad0616 <tad0616@gmail.com>
2024-11-08 15:14:24 +0800

    修改版號

--- xoops_version.php
+++ xoops_version.php
@@ -27,7 +27,7 @@ $modversion['module_website_name'] = 'XOOPS EZGO';
 $modversion['author_website_url'] = 'https://www.tad0616.net';
 $modversion['author_website_name'] = 'Tad';
 $modversion['min_php'] = '5.4';
-$modversion['min_xoops'] = '2.5.7';
+$modversion['min_xoops'] = '2.5.10';
 $modversion['min_db'] = [
     'mysql' => '5.0.7',
     'mysqli' => '5.0.7',
@@ -43,7 +43,7 @@ $modversion['paypal'] = [
 
 //---資料表架構---//
 $modversion['sqlfile']['mysql'] = 'sql/mysql.sql';
-$modversion['tables'][1] = 'tadtools_setup';
+$modversion['tables'] = ['tadtools_setup'];
 
 //---啟動後台管理界面選單---//
 $modversion['system_menu'] = 1;
@@ -65,102 +65,20 @@ $modversion['onUninstall'] = 'include/onUninstall.php';
 $i = 1;
 $modversion['templates'][$i]['file'] = 'tadtools_adm_index.tpl';
 $modversion['templates'][$i]['description'] = 'tadtools_adm_index.tpl';
+$modversion['config'] = [
+    ['name' => 'auto_charset', 'title' => '_MI_TADTOOLS_TITLE5', 'description' => '_MI_TADTOOLS_DESC5', 'formtype' => 'yesno', 'valuetype' => 'int', 'default' => 1],
+    ['name' => 'uploadcare_publickey', 'title' => '_MI_TADTOOLS_TITLE8', 'description' => '_MI_TADTOOLS_DESC8', 'formtype' => 'textbox', 'valuetype' => 'text', 'default' => ''],
+    ['name' => 'use_codemirror', 'title' => '_MI_TADTOOLS_USE_CODEMIRROR', 'description' => '_MI_TADTOOLS_USE_CODEMIRROR_DESC', 'formtype' => 'yesno', 'valuetype' => 'int', 'default' => 1],
+    ['name' => 'image_max_width', 'title' => '_MI_TADTOOLS_IMAGE_MAX_WIDTH', 'description' => '_MI_TADTOOLS_IMAGE_MAX_WIDTH_DESC', 'formtype' => 'textbox', 'valuetype' => 'int', 'default' => 1920],
+    ['name' => 'image_max_height', 'title' => '_MI_TADTOOLS_IMAGE_MAX_HEIGHT', 'description' => '_MI_TADTOOLS_IMAGE_MAX_HEIGHT_DESC', 'formtype' => 'textbox', 'valuetype' => 'int', 'default' => 1920],
+    ['name' => 'insert_spacing', 'title' => '_MI_TADTOOLS_INSERT_SPACING', 'description' => '_MI_TADTOOLS_INSERT_SPACING_DESC', 'formtype' => 'yesno', 'valuetype' => 'int', 'default' => 0],
+    ['name' => 'linkify', 'title' => '_MI_TADTOOLS_LINKIFY', 'description' => '_MI_TADTOOLS__MI_TADTOOLS_LINKIFY_DESC', 'formtype' => 'yesno', 'valuetype' => 'int', 'default' => 1],
+    ['name' => 'pdf_force_dl', 'title' => '_MI_TADTOOLS_PDF_FORCE_DL', 'description' => '_MI_TADTOOLS__MI_TADTOOLS_PDF_FORCE_DL_DESC', 'formtype' => 'yesno', 'valuetype' => 'int', 'default' => 0],
+    ['name' => 'test_mode', 'title' => '_MI_TADTOOLS_TEST_MODE', 'description' => '_MI_TADTOOLS_TEST_MODE_DESC', 'formtype' => 'yesno', 'valuetype' => 'int', 'default' => 1],
+    ['name' => 'facebook_app_id', 'title' => '_MI_TADTOOLS_FACEBOOK_APP_ID', 'description' => '_MI_TADTOOLS_FACEBOOK_APP_ID_DESC', 'formtype' => 'textbox', 'valuetype' => 'text', 'default' => ''],
+];
 
-//---偏好設定---//
-$i = 1;
-$modversion['config'][$i]['name'] = 'auto_charset';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_TITLE5';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS_DESC5';
-$modversion['config'][$i]['formtype'] = 'yesno';
-$modversion['config'][$i]['valuetype'] = 'int';
-$modversion['config'][$i]['default'] = '1';
-
-$i++;
-$modversion['config'][$i]['name'] = 'uploadcare_publickey';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_TITLE8';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS_DESC8';
-$modversion['config'][$i]['formtype'] = 'textbox';
-$modversion['config'][$i]['valuetype'] = 'text';
-$modversion['config'][$i]['default'] = '';
-
-$i++;
-$modversion['config'][$i]['name'] = 'use_codemirror';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_USE_CODEMIRROR';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS_USE_CODEMIRROR_DESC';
-$modversion['config'][$i]['formtype'] = 'yesno';
-$modversion['config'][$i]['valuetype'] = 'int';
-$modversion['config'][$i]['default'] = '1';
-
-$i++;
-$modversion['config'][$i]['name'] = 'image_max_width';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_IMAGE_MAX_WIDTH';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS_IMAGE_MAX_WIDTH_DESC';
-$modversion['config'][$i]['formtype'] = 'textbox';
-$modversion['config'][$i]['valuetype'] = 'int';
-$modversion['config'][$i]['default'] = '1920';
-
-$i++;
-$modversion['config'][$i]['name'] = 'image_max_height';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_IMAGE_MAX_HEIGHT';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS_IMAGE_MAX_HEIGHT_DESC';
-$modversion['config'][$i]['formtype'] = 'textbox';
-$modversion['config'][$i]['valuetype'] = 'int';
-$modversion['config'][$i]['default'] = '1920';
-
-$i++;
-$modversion['config'][$i]['name'] = 'insert_spacing';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_INSERT_SPACING';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS_INSERT_SPACING_DESC';
-$modversion['config'][$i]['formtype'] = 'yesno';
-$modversion['config'][$i]['valuetype'] = 'int';
-$modversion['config'][$i]['default'] = '0';
-
-$i++;
-$modversion['config'][$i]['name'] = 'linkify';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_LINKIFY';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS__MI_TADTOOLS_LINKIFY_DESC';
-$modversion['config'][$i]['formtype'] = 'yesno';
-$modversion['config'][$i]['valuetype'] = 'int';
-$modversion['config'][$i]['default'] = '1';
-
-$i++;
-$modversion['config'][$i]['name'] = 'pdf_force_dl';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_PDF_FORCE_DL';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS__MI_TADTOOLS_PDF_FORCE_DL_DESC';
-$modversion['config'][$i]['formtype'] = 'yesno';
-$modversion['config'][$i]['valuetype'] = 'int';
-$modversion['config'][$i]['default'] = '0';
-
-$i++;
-$modversion['config'][$i]['name'] = 'test_mode';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_TEST_MODE';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS_TEST_MODE_DESC';
-$modversion['config'][$i]['formtype'] = 'yesno';
-$modversion['config'][$i]['valuetype'] = 'int';
-$modversion['config'][$i]['default'] = '1';
-
-$i++;
-$modversion['config'][$i]['name'] = 'facebook_app_id';
-$modversion['config'][$i]['title'] = '_MI_TADTOOLS_FACEBOOK_APP_ID';
-$modversion['config'][$i]['description'] = '_MI_TADTOOLS_FACEBOOK_APP_ID_DESC';
-$modversion['config'][$i]['formtype'] = 'textbox';
-$modversion['config'][$i]['valuetype'] = 'text';
-$modversion['config'][$i]['default'] = '';
-
-$i = 0;
-$modversion['blocks'][$i]['file'] = 'tadtools_qrcode.php';
-$modversion['blocks'][$i]['name'] = _MI_TADTOOLS_QRCODE_BLOCK_NAME;
-$modversion['blocks'][$i]['description'] = _MI_TADTOOLS_QRCODE_BLOCK_DESC;
-$modversion['blocks'][$i]['show_func'] = 'tadtools_qrcode';
-$modversion['blocks'][$i]['template'] = 'tadtools_qrcode_block.tpl';
-$modversion['blocks'][$i]['edit_func'] = 'tadtools_qrcode_edit';
-$modversion['blocks'][$i]['options'] = '120';
-
-$i++;
-$modversion['blocks'][$i]['file'] = 'tadtools_app.php';
-$modversion['blocks'][$i]['name'] = _MI_TADTOOLS_APP_BLOCK_NAME;
-$modversion['blocks'][$i]['description'] = _MI_TADTOOLS_APP_BLOCK_DESC;
-$modversion['blocks'][$i]['show_func'] = 'tadtools_app';
-$modversion['blocks'][$i]['template'] = 'tadtools_app_block.tpl';
-$modversion['blocks'][$i]['edit_func'] = 'tadtools_app_edit';
-$modversion['blocks'][$i]['options'] = '120|v';
+$modversion['blocks'] = [
+    ['file' => 'tadtools_qrcode.php', 'name' => _MI_TADTOOLS_QRCODE_BLOCK_NAME, 'description' => _MI_TADTOOLS_QRCODE_BLOCK_DESC, 'show_func' => 'tadtools_qrcode', 'template' => 'tadtools_qrcode_block.tpl', 'edit_func' => 'tadtools_qrcode_edit', 'options' => '120'],
+    ['file' => 'tadtools_app.php', 'name' => _MI_TADTOOLS_APP_BLOCK_NAME, 'description' => _MI_TADTOOLS_APP_BLOCK_DESC, 'show_func' => 'tadtools_app', 'template' => 'tadtools_app_block.tpl', 'edit_func' => 'tadtools_app_edit', 'options' => '120|v'],
+];
